5d119e4dbd45ba00c7d152570e6e5323e64fa3cd
Inas Arfaoui <inassarff@gmail.com>
2025-09-11 16:34:27 +0200

    Add 7-multi_c.js script

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const x = parseInt(process.argv[2], 10);
+
+if (isNaN(x) || x <= 0) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
+
